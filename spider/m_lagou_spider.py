296fa01d0f3e0b5881e0643f0f1f57df08fbef2c
Lincoln <lincoln@hiredbull.com>
2018-06-17 11:16:49 +0800

    做了注释
    增加了一些注释，然后修改了对 jobs 的遍历的 for 的定义。

--- spider/m_lagou_spider.py
+++ spider/m_lagou_spider.py
@@ -1,6 +1,15 @@
 """
 a web spider for mobile lagou
+多行注释，
+
+很多很多行
+
 """
+
+# 单行注释
+
+# 指明编码方式
+
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 
@@ -8,31 +17,38 @@ a web spider for mobile lagou
 import os
 import sys
 import time
-
 import requests
 
-sys.path.append('../')
+sys.path.append('../') # 纯粹是为了下一行好用才加的这一句
 
 from util.file_reader import parse_job_xml
 
 # 将模块路径加到当前模块扫描的路径里
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
+
 from spider.jobdetail_spider import crawl_job_detail
+
 import pandas as pd
 from util import log
 from config.config import *
+
+# 以上，翻来覆去就是为了导入各种乱八七糟
+
 
 try:
     # 导入parse模块并命名为parse
     from urllib import parse as parse
 except:
     import urllib as parse
-
     sys.reload()
     sys.setdefaultencoding('utf-8')
 
+
+
 # 从拉勾网爬取职位信息
 def crawl_jobs(positionName):
+    # 定义一个列表
     JOB_DATA = list()
     # 创建max_page_number对象，返回最大页数
     max_page_number = get_max_pageNo(positionName)
@@ -92,10 +108,16 @@ def get_cookies():
 # 返回某个工作的最大页数
 def get_max_pageNo(positionName):
     # cookies为首次访问的cookies
+    # cookie 不是WYY 说的甜点，是服务器去识别一个客户的基本的方法
     cookies = get_cookies()
     # url请求
+    # %E5 这是个什么东东呢？
+    # 确认过眼神，下面这个地址，确实可以访问
+    # https://m.lagou.com/search.json?city=全国&positionName=机器学习&pageNo=1&pageSize=15
     request_url = 'https://m.lagou.com/search.json?city=%E5%85%A8%E5%9B%BD&positionName=' + parse.quote(
         positionName) + '&pageNo=1&pageSize=15'
+
+    # 开始，伪装自己
     headers = {
         'Accept': 'application/json',
         'Accept-Encoding': 'gzip, deflate, sdch',
@@ -106,8 +128,12 @@ def get_max_pageNo(positionName):
         'X-Requested-With': 'XMLHttpRequest',
         'Connection': 'keep-alive'
     }
+
     # 发送请求
     response = requests.get(request_url, headers=headers, cookies=cookies, timeout=10)
+    
+    # 上面代码执行结束之后，response-响应里面就包含了，我们需要的东西
+
     print("Getting data from %s successfully~" % positionName + request_url)
     if response.status_code == 200:
         max_page_no = int(int(response.json()['content']['data']['page']['totalCount']) / 15 + 1)
@@ -124,10 +150,15 @@ def get_max_pageNo(positionName):
 
 # 爬取职位信息，将内容保存在当前目录的data文件夹下
 if __name__ == '__main__':
+    # 读取这个文件，把定义好的要爬取的职位的信息加载进来
     craw_job_list = parse_job_xml('../config/job.xml')
-    for _ in craw_job_list:
+    # 上一行代码执行结束之后，就会有8个职位在 craw_job_list
+    
+    # 挨个遍历
+
+    for job in craw_job_list:
         # 创建joblist对象
-        joblist = crawl_jobs(_)
+        joblist = crawl_jobs(job)
         col = [
             u'职位编码',
             u'职位名称',
@@ -141,4 +172,4 @@ if __name__ == '__main__':
         df = pd.DataFrame(joblist, columns=col)
         path = "./data/"
         # 文件保存路径
-        df.to_excel(path + _ + ".xlsx", sheet_name=_, index=False)
+        df.to_excel(path + job + ".xlsx", sheet_name=job, index=False)
